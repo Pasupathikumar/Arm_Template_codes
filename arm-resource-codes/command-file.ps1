6b2d60c143ceb9bb0f29398a8fb493eb20136219
Pasupathikumar Selvaraj <kumarmarimuthu99@gmail.com>
2024-09-21 21:12:00 +0530

    commit

--- arm-resource-codes/command-file.ps1
+++
@@ -1,6 +0,0 @@
-az group create --name arm-template --location 'southindia'
-az deployment group create --resource-group arm-template --template-file azure-kubernetes-cluster.json --parameters @azure-template.paramaters.json
-az deployment group create --resource-group arm-template --template-file azure-multi-vm.json --parameters @azure-template.paramaters.json
-az deployment group create --resource-group arm-template --template-file azure-web-app.json --parameters @azure-template.paramaters.json
-az deployment group create --resource-group arm-template --template-file azure-storage-account.json --parameters @azure-template.paramaters.json
-
